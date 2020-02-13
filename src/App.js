47065eba79731dbace82e86a41b2c0a10fa281c8
matrix <matrixediting_domain@protonmail.com>
2020-02-13 23:25:17 +0000

    Reduce font import size.

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ function App() {
 
 
   const GlobalStyle = createGlobalStyle`
-      @import url('https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap');
-      @import url('https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap');
+      @import url('https://fonts.googleapis.com/css?family=Raleway:200,300,400,500,600,700,800,&display=swap');
 
       :root{
           font-size: 6.25%;  /** Make rem value same as px equivalent. **/
